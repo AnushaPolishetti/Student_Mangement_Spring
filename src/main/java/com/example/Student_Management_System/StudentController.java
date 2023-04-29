de564083f1b7995d7d4dffb2fab8005a6d04729f
unknown <polishettianusha@gmail.com>
2023-04-29 19:57:27 +0530

    first commit

---
+++ src/main/java/com/example/Student_Management_System/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.Student_Management_System;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController // In this class we will be writing api endpoints
+public class StudentController {
+
+    HashMap<Integer,Student> hashMap = new HashMap<>();
+    @PostMapping("/registerStudent")
+    public void addStudent(@RequestBody Student student){
+
+
+        int key = student.getRollNo();
+        hashMap.put(key,student);
+    }
+
+
+    @GetMapping("/getStudentInfo")
+    public Student getStudent(@RequestParam("rollNo")Integer rollNo){
+        Student student = hashMap.get(rollNo);
+        return student;
+    }
+
+}
